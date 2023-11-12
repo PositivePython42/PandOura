02284dd06a1ce8314df9874facc48a999f1dcc43
Positive Python <sean@positivepython.co.uk>
2023-11-12 11:14:38 +0000

    Add files via upload

--- pandoura.py
+++ pandoura.py
@@ -2,22 +2,21 @@
 
 import pandas as pd
 import streamlit as st
-import datetime
 
 st.set_page_config(layout="wide")
 st.title('**PandOura** - Data From My Oura Ring')
 
 with st.expander('About this app'):
-    st.write('You can use this app to analyse the data from your Oura ring, uploading using the data uplader below.  To download your Oura data;'
-             '1. Use your browser to go to https://cloud.ouraring.com/trends'
-             '2. Chose the data range you want to download. You can do this be selecting the data range on the top graph on this page or using the drop down menu with the dates on it.'
-             '3. In the top right hand side of the screen press the "Download Data" button'
+    st.write('You can use this app to analyse the data from your Oura ring, uploading using the data uplader below.  To download your Oura data;\n'
+             '1. Use your browser to go to https://cloud.ouraring.com/trends\n'
+             '2. Chose the data range you want to download. You can do this be selecting the data range on the top graph on this page or using the drop down menu with the dates on it\n'
+             '3. In the top right hand side of the screen press the "Download Data" button\n'
              '4. Chose "Select All" from the drop down menu'
              )
     st.write('Please suggest a feature.  Email me at sean@positivepython.co.uk, or raise an issue on GitHub https://github.com/PositivePython42/PandOura/issues')
 
-"""Upload and perform essential tidy up functions on the data"""
-"""Upload the data"""
+#Upload and perform essential tidy up functions on the data
+#Upload the data
 st.header('Upload your data here')
 st.subheader('Please use csv format')
 uploaded_file = st.file_uploader("Chose a file")
@@ -27,11 +26,11 @@ if uploaded_file is not None:
 else:
     st.info('Upload a file')
 
-"""Turn the time/date based data into something Pandas can understand properly"""
+#Turn the time/date based data into something Pandas can understand properly
 df['date'] = pd.to_datetime(df['date'])
 
 
-"""Replace all the blank data items with a code Pandas uses to exclude the date, otherwise it skews the analysis"""
+#Replace all the blank data items with a code Pandas uses to exclude the date, otherwise it skews the analysis#
 
 
 
